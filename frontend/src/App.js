147fb89a38151f0b25666bc2fe40c9d78e4fdf1b
Alyssa Hope <alyssamhope@gmail.com>
2018-07-16 11:51:46 -0700

    adds route for login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import AllApartments from './pages/apartments/All'
 import CreateApartment from './pages/apartments/Create'
 import ShowApartment from './pages/apartments/Show'
+import Login from './components/Login';
 
 class App extends Component {
 	render() {
@@ -14,6 +15,7 @@ class App extends Component {
 					<Route exact path="/apartments" component={AllApartments} />
 					<Route path="/apartments/new" component={CreateApartment} />
 					<Route path="/apartments/:id" component={ShowApartment} />
+					<Route path="/login" component={Login} />
 					<Route exact path="/" component={AllApartments}/>
 				</Switch>
 			</Router>
